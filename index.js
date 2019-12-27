aadd8ca38f6b0fd0fcc8362b0d54fc64024a6c5d
Aditya Tanguturi <adityatanguturi@Adityas-MacBook-Pro.local>
2019-12-26 19:40:30 -0500

    Basic idea implemented with concat method

---
+++ index.js
@@ -0,0 +1,23 @@
+import ArraySideCodes from './array-sidecodes';
+import StringSideCodes from './string-sidecodes';
+import NumberSideCodes from './number-sidecodes';
+import ObjectSideCodes from './object-sidecodes';
+
+const arraySideCodes = new ArraySideCodes();
+const stringSideCodes = new StringSideCodes();
+const numberSideCodes = new NumberSideCodes();
+const objectSideCodes = new ObjectSideCodes();
+
+export function concat(arg1, arg2) {
+  var typeMethodRelation = {
+    'number': numberSideCodes,
+    'object': objectSideCodes,
+    'string': stringSideCodes,
+    'array':  arraySideCodes,
+  };
+  return typeMethodRelation[typeof arg1].concat.call(arg1, arg2);
+}
+
+export function add(arg1, arg2) {
+  return numberSideCodes.add(arg1, arg2);
+}
