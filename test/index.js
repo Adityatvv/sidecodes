3f5ccd9cc0e1931be1897c7c6333f140fc67f49a
Aditya Tanguturi <adityatanguturi@Adityas-MacBook-Pro.local>
2019-12-31 21:20:25 -0500

    Add chai

--- test/index.js
+++ test/index.js
@@ -1,26 +1,30 @@
 import assert from 'assert';
+import { expect } from 'chai';
 import { getType, add, concat } from '../src/index';
 
 describe('getType', () => {
   it('should return array when the [1,2, 3] is passed', () => {
-    assert.equal(getType([1, 2, 3]), 'array');
+    expect(getType([1, 2, 3])).to.be.equal('array');
   });
 });
 
 describe('#concat', () => {
   it('should return foobar when the values are foo and bar', () => {
-    assert.equal(concat('foo', 'bar'), 'foobar');
+   expect(concat('foo', 'bar')).to.be.equal('foobar');
   });
   it('should return [1,2,3,4] when the values are [1,2] and [3,4]', () => {
-    assert.equal(JSON.stringify(concat([1,2], [3,4])), JSON.stringify([1, 2, 3, 4]));
+    expect(concat([1,2], [3,4])).to.be.deep.equal([1, 2, 3, 4]);
+  });
+  it('should return merged object', () => {
+    expect(concat({ a: 1}, {b: 2})).to.be.deep.equal({a: 1, b:2});
   });
 });
 
 describe('#add', () => {
   it('should return 3 when the values are 1 and 2', () => {
-    assert.equal(add(1, 2), 3);
+    expect(add(1, 2)).to.be.equal(3);
   });
   it('should return "foo bar" when the values are foo and bar', () => {
-    assert.equal(add('foo', 'bar'), 'foo bar');
+    expect(add('foo', 'bar')).to.be.equal('foo bar');
   });
 });
